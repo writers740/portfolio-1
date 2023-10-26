af939d762248970523ecfc4c41dca8a48d8b8c09
writers740 <137925104+writers740@users.noreply.github.com>
2023-10-26 23:05:55 +0300

    Update spin.js

--- js/spin.js
+++ js/spin.js
@@ -5,7 +5,7 @@
   loadbtn.addEventListener('mouseover', stary)
   function stary(){
     let alertbtn=    document.getElementById('alertbtn')
-    if( alertbtn.textContent==''){
+    if( alertbtn.textContent==' '){
       alertbtn.textContent="download Evansdev's c.v click here..."
       alertbtn.style.backgroundColor='white'
     }else{
@@ -25,5 +25,19 @@ icon.addEventListener('mouseout',()=>{
   icon.style.opacity='1' 
 })
 })
-  
+ let submit =document.querySelector(".submit")
+let inputs = document.querySelectorAll('.form-control')
+
+submit.addEventListener("click", formFunction())
+
+function formFunction(){
+inputs.forEach(input => {
+
+  if(input.value === ""){
+    input.style.borderColor = "red"
+    input.style.outline = "2px"
+  document.querySelectorAll('#please').textContent = "please enter ! something!!"
+    
+  }
+})}
  
